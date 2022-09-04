c334b4058722d1e3407e52832eb37640d330c5ba
nikoevi87 <76126175+nikoevi87@users.noreply.github.com>
2022-09-04 12:18:32 -0300

    CON FUILTRO FUNCIONAL -

--- lavaderos_online/lavaderos/views.py
+++ lavaderos_online/lavaderos/views.py
@@ -2,6 +2,7 @@ from unicodedata import name
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import NewUserForm, NewLavaderoForm, NewTarifaForm
+from .models import Lavadero
 from django.contrib.auth import login
 from django.contrib import messages
 from lavaderos.models import *
@@ -15,16 +16,37 @@ def inicio(request):
 
 #NICO
 def basic(request):
-    return render(request,'basic.html',{})
+    featured_filter = 'T'
+    if request.GET.get('estado'):
+        featured_filter = request.GET.get('estado')
+        if featured_filter == 'T':
+            lavadero = Lavadero.objects.exclude(estado='I')
+        else:
+            lavadero = Lavadero.objects.filter(estado=featured_filter)
+    else:
+        lavadero = Lavadero.objects.exclude(estado='I')
+    return render(request,'basic.html',{'lavaderolist':lavadero,'estadoselect':featured_filter})
     
     
 #NICO
-def lavadero(request):
-    return render(request,'lavadero.html',{})
+def lavadero(request,id):
+    print('ENTRO A LAVADERO')
+    try:
+        lavadero = Lavadero.objects.get(pk=id)
+    except Lavadero.DoesNotExist:
+        lavadero = None
+    if lavadero:
+        return render(request, 'lavadero.html', {'lavadero': lavadero})
+    else:
+        print(lavadero)
+        return redirect("lavaderos")
+
+
 
 #NICO
 @login_required(login_url='/cuentas/login/')
 def registroLavadero(request):
+
     user = request.user
     form = NewLavaderoForm()
     formulario = NewTarifaForm()
