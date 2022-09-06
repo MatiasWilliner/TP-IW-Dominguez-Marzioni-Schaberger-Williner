3b01f78c975bac1079db6486bb6d56c83c03d819
JuaniScha <84474999+JuaniScha@users.noreply.github.com>
2022-09-05 23:39:23 -0300

    Del módulos innecesarios

--- lavaderos_online/lavaderos/models.py
+++ lavaderos_online/lavaderos/models.py
@@ -1,8 +1,4 @@
-from asyncio import constants
-from asyncio.windows_events import NULL
-from tkinter import CASCADE
 from django.db import models
-from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 from django.urls import reverse
 
