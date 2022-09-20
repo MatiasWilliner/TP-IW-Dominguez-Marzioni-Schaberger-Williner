963c423d66c196bd5190e4ee2284daf030cc1770
JuaniScha <84474999+JuaniScha@users.noreply.github.com>
2022-09-20 15:15:13 -0300

    Agrego redirect milavadero a register
    En caso de que no tenga lavadero y se quiera acceder a /milavadero, redirecciona al registro de lavadero.

--- lavaderos_online/lavaderos/views.py
+++ lavaderos_online/lavaderos/views.py
@@ -166,7 +166,7 @@ def miLavadero(request):
             
         return render(request, 'milavadero.html', {'tarifa_form':formset_tarifa, 'horario_form':formset_horario, 'estado_form':formset_estado})
     else:
-        return redirect("lavaderos")      
+        return redirect("registrolavadero")      
 
 def activateEmail(request, user, to_email):
     mail_subject = 'Confirma tu cuenta de Lavadero Online!.'
